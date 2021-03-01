f1e9f250830084987c3ec92881d2eb288fe3d243
Natorden <71587437+Natorden@users.noreply.github.com>
2021-03-01 16:44:57 +0100

    attempt on placing played image

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -2,9 +2,12 @@ package rps.gui.controller;
 
 // Java imports
 import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
@@ -29,6 +32,8 @@ public class GameViewController implements Initializable {
     @FXML
     private JFXButton scissor;
 
+    //boolean rock false;
+
     /**
      * Initializes the controller class.
      */
@@ -38,6 +43,7 @@ public class GameViewController implements Initializable {
     }
 
     public void playsRock() {
+
     }
 
     public void playsPaper() {
@@ -45,4 +51,43 @@ public class GameViewController implements Initializable {
 
     public void playsScissor() {
     }
+
+    public void onAction(ActionEvent ae) {
+        Image image;
+        Button b = (Button) ae.getSource();
+        if (b.equals(rock)) {
+            image = new Image(getClass().getResourceAsStream("../view/Images/Rock.png"));
+            playerImageView.setImage(image);
+        } else if (b.equals(paper)) {
+            image = new Image(getClass().getResourceAsStream("../view/Images/Paper.png"));
+            playerImageView.setImage(image);
+        } else if (b.equals(paper)) {
+            image = new Image(getClass().getResourceAsStream("../view/Images/Scissors.png"));
+            playerImageView.setImage(image);
+        } else {
+            return;
+        }
+    }
 }
+
+/*
+    public void setPlayedHandAsImage(){
+        Image image;
+        if(rock.isPressed()){
+            image = new Image(getClass().getResourceAsStream("../view/Images/Rock.png"));
+            playerImageView.setImage(image);
+        }
+        else if(paper.isPressed()){
+            image = new Image(getClass().getResourceAsStream("../view/Images/Paper.png"));;
+            playerImageView.setImage(image);
+        }
+        else if(scissor.isPressed()){
+            image = new Image(getClass().getResourceAsStream("../view/Images/Scissors.png"));;
+            playerImageView.setImage(image);
+        }
+        else{
+            return;
+        }
+    }
+*/
+
